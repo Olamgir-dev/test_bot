d13cfd7b0813bffe1c41cbd41a452ce505bead0d
Olamgir <121297947+Olamgir-dev@users.noreply.github.com>
2024-09-23 12:14:26 +0500

    add

--- bot.js
+++ bot.js
@@ -8,9 +8,12 @@ bot.start((ctx) => {
       keyboard: [
         [
           { text: 'Web App', web_app: { url: 'https://appfyl.vercel.app' } }
+        ], [
+          { text: 'Share Phone Number', request_contact: true },
+          { text: 'Location', request_location: true }
         ]
       ],
-    //   resize_keyboard: true, // Optional: Makes the keyboard more compact
+      //   resize_keyboard: true, // Optional: Makes the keyboard more compact
     },
   });
 });
